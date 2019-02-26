554029998311fd147d3e8a32081b902d865ac478
HunterSun2008 <sht@palliums.org>
2019-02-26 14:57:27 +0800

    modified file configure.ac

--- cleanup.sh
+++ cleanup.sh
@@ -3,3 +3,4 @@ rm -f config.guess config.h.in config.h.in~ config.sub configure compile
 rm -f install-sh ltmain.sh 
 rm -r -f debug release m4 depcomp missing
 rm -r -f src/Makefile.in 
+rm -f ar-lib
